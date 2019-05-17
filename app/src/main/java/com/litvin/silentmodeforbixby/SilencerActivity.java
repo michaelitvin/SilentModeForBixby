13dff7285599c3af2b94a484129b595bd5520d1d
Michael Litvin <michael.litvin@gmail.com>
2019-05-18 00:19:03 +0300

    Add toasts and error handling

--- app/src/main/java/com/litvin/silentmodeforbixby/SilencerActivity.java
+++ app/src/main/java/com/litvin/silentmodeforbixby/SilencerActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SilencerActivity extends AppCompatActivity {
 
@@ -11,11 +12,17 @@ public class SilencerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AudioManager am = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
-        if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
-            am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-        } else {
-            am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        try {
+            AudioManager am = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+            if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Toast.makeText(this, "Vibrate mode", Toast.LENGTH_SHORT).show();
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                Toast.makeText(this, "Ringer sound enabled", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(this, "Could not set ringer: " + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
 
         finish();
